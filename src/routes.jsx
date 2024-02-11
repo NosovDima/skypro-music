bb6490798a4af28aa60ff4e3e71c9223a3b6ebf3
DimaNosov <fitnosov@gmail.com>
2024-02-11 19:51:22 +0700

    Login null

--- src/routes.jsx
+++ src/routes.jsx
@@ -6,8 +6,12 @@ import { Login } from "./pages/Login/Login.jsx"
 import { Registration } from "./pages/Registration/Registration.jsx"
 import { MyPlaylist } from "./pages/Myplaylist/MyPlaylist.jsx"
 import { Main } from "./components/Main.jsx"
+import { useState } from 'react'
 
-export const AppRoutes = ({ user, onClick }) => {
+export const AppRoutes = ({ onClick }) => {
+
+    const [user, setUser] = useState(localStorage.getItem('user'))
+
     return (
         <Routes>
             <Route path="/Login" element={<Login onClick={onClick} />} />
@@ -15,7 +19,7 @@ export const AppRoutes = ({ user, onClick }) => {
             <Route element={<ProtectedRoute isAllowed={Boolean(user)}/>}>
             <Route path="/MyPlaylist" element={<MyPlaylist />} />
             <Route path="/Category/:id" element={<Category />} />
-            <Route path="/" element={<Main />} />
+            <Route path="/" element={<Main setUser={setUser} />} />
             </Route>
             <Route path="*" element={<NotFound />} />
         </Routes>
