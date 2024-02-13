58a373def21cc4fe6464698ad988d19cf3922e1e
DimaNosov <fitnosov@gmail.com>
2024-02-14 03:10:04 +0700

    готово

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,20 +1,20 @@
 import { Route, Routes } from "react-router-dom"
 import { NotFound } from "./pages/Error/NotFound.jsx"
-import { Category } from "./pages/Category/Category.jsx"
+import { Category } from "./pages/Category/category.jsx"
 import { ProtectedRoute } from "./pages/ProtectedRoute.jsx"
-import { Login } from "./pages/Login/Login.jsx"
-import { Registration } from "./pages/Registration/Registration.jsx"
-import { MyPlaylist } from "./pages/Myplaylist/MyPlaylist.jsx"
+import { Login } from "./pages/Login/login.jsx"
+import { Registration } from "./pages/Registration/registration.jsx"
+import { MyPlaylist } from "./pages/Myplaylist/myPlaylist.jsx"
 import { Main } from "./components/Main.jsx";
 
 export const AppRoutes = ({ user, setUser, onClick }) => {
   return (
     <Routes>
-      <Route path="/Login" element={<Login onClick={onClick} />} />
-      <Route path="/Registration" element={<Registration />} />
+      <Route path="/login" element={<Login onClick={onClick} />} />
+      <Route path="/registration" element={<Registration />} />
       <Route element={<ProtectedRoute isAllowed={Boolean(user)} />}>
-        <Route path="/MyPlaylist" element={<MyPlaylist />} />
-        <Route path="/Category/:id" element={<Category />} />
+        <Route path="/myPlaylist" element={<MyPlaylist />} />
+        <Route path="/category/:id" element={<Category />} />
         <Route path="/" element={<Main setUser={setUser} />} />
       </Route>
       <Route path="*" element={<NotFound />} />
